938e41d7f3e5b88376b097f3805f83a0b343e892
acepele <47071340+acepele@users.noreply.github.com>
2021-05-23 11:23:58 -0500

    Add files via upload

---
+++ song.py
@@ -0,0 +1,54 @@
+# You are my sunshine
+
+print("You are my sunshine, by Johnny Cash\n")
+
+print("The other night dear,as I lay sleeping")
+print("I dreamed I held you in my arms.")
+print("But when I awoke,dear, I was mistaken")
+print("So I hung my head and I cried.\n")
+
+
+#   ref
+print("You are my sunshine,my only sunshine")
+print("You make me happy when skies are gray ")
+print("You'll never know dear,how much I love you")
+print("Please don't take my sunshine away\n")
+
+
+print("I'll always love you and make you happy,")
+print("If you will only say the same.")
+print("But if you leave me and love another,")
+print("You'll regret it all some day:\n")
+
+#   ref
+print("You are my sunshine,my only sunshine")
+print("You make me happy when skies are gray ")
+print("You'll never know dear,how much I love you")
+print("Please don't take my sunshine away\n")
+
+
+print("You told me once,dear,you really loved me")
+print("And no one else could come between.")
+print("But not you've left me and love another;")
+print("You have shattered all of my dream:\n")
+
+print("You are my sunshine,my only sunshine")
+print("You make me happy when skies are gray ")
+print("You'll never know dear,how much I love you")
+print("Please don't take my sunshine away\n")
+
+print("In all my dreams,dear,you seem to leave")
+print("me")
+print("When I awake my poor heart pains.")
+print("So when you come back and make me")
+print("happy")
+print("I will forgive you dear,I'll take all the blame.\n")
+
+#  ref
+print("You are my sunshine,my only sunshine")
+print("You make me happy when skies are gray ")
+print("You'll never know dear,how much I love you")
+print("Please don't take my sunshine away\n")
+
+
+print("Songwriters: Jimmie Davis")
