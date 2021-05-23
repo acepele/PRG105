ba91a7eae40c0982f42dbc5340e68af190f54682
acepele <47071340+acepele@users.noreply.github.com>
2021-05-23 14:05:05 -0500

    Add files via upload

---
+++ main2song.py
@@ -0,0 +1,63 @@
+# Variables for MadLibs program
+noun = input("Enter a noun to describe the time: ")
+noun1 = input("Enter a noun for your love one: ")
+action = input("Enter a verb: ")
+body = input("Enter a part of body: ")
+feeling = input("Enter an emotional state: ")
+color = input("Enter a color: ")
+verb = input("Enter a loved verb: ")
+atmosphere = input("Enter a noun: ")
+affection = input("Enter a noun: ")
+
+# Printing the mad libs result
+print("You are my " + noun1 + ", by Johnny Cash\n")
+print("The other " + noun + " " + affection + " ,as I " + action + " sleeping")
+print("I dreamed I " + action + " you in my " + body + ".")
+print("But when I " + action + "," + affection + ", I was mistaken")
+print("So I " + action + " my " + body + " and I " + feeling + ".\n")
+
+
+#   ref
+print("You are my " + noun1 + ",my only " + noun1 + " ")
+print("You make me " + feeling + " when " + atmosphere + " are " + color + " ")
+print("You'll never know " + affection + ",how much I " + verb + " you")
+print("Please don't take my " + noun1 + " away\n")
+
+
+print("I'll always " + verb + " you and make you " + feeling + ",")
+print("If you will only " + action + " the same.")
+print("But if you " + action + " me and " + verb + " another,")
+print("You'll regret it all some " + noun + ":\n")
+
+#   ref
+print("You are my " + noun1 + ",my only " + noun1 + " ")
+print("You make me " + feeling + " when " + atmosphere + " are " + color + " ")
+print("You'll never know " + affection + ",how much I " + verb + " you")
+print("Please don't " + action + " my " + noun1 + " away\n")
+
+
+print("You " + action + " me once," + affection + ",you really loved me")
+print("And no one else could come between.")
+print("But not you've " + action + "  me and " + verb + " another;")
+print("You have " + action + " all of my dream:\n")
+
+print("You are my " + noun1 + ",my only " + noun1 + " ")
+print("You make me " + feeling + " when " + atmosphere + " are " + color + " ")
+print("You'll never know " + affection + ",how much I love you")
+print("Please don't take my " + noun1 + " away\n")
+
+print("In all my dreams," + affection + ",you seem to " + action + " ")
+print("me")
+print("When I " + action + " my poor " + body + " pains.")
+print("So when you " + action + " and make me")
+print(" + feeling + ")
+print("I will " + action + " you " + affection + ",I'll " + action + " all the blame.\n")
+
+#  ref
+print("You are my " + noun1 + ",my only " + noun1 + " ")
+print("You make me " + feeling + " when " + atmosphere + " are " + color + " ")
+print("You'll never know " + affection + ",how much I love you")
+print("Please don't take my " + noun1 + " away\n")
+
+
+print("Songwriters: Jimmie Davis")
