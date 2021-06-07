8984e9686d4236cd044a31cb47c090758fd57855
acepele <47071340+acepele@users.noreply.github.com>
2021-06-07 10:16:44 -0500

    Add files via upload

--- sales1.py
+++ sales1.py
@@ -1,13 +1,6 @@
-sale_1 = float(input("What were the total sales for Sunday:"))
-sale_2 = float(input("What were the total sales for Monday:"))
-sale_3 = float(input("What were the total sales for Tuesday:"))
-sale_4 = float(input("What were the total sales for Wednesday:"))
-sale_5 = float(input("What were the total sales for Thursday:"))
-sale_6 = float(input("What were the total sales for Friday:"))
-sale_7 = float(input("What were the total sales for Saturday:"))
-
-total_sales_for_the_week = sale_1 + sale_2 + sale_3 + sale_4 + sale_5 + sale_6 + sale_7
-average_sales_per_day = total_sales_for_the_week / 7
-
-print("The total amount of sales for the week was: $" + format(total_sales_for_the_week,",.2f"))
-print("The average amount of sales per day was: $" + format(average_sales_per_day,",.2f"))
+total = 0
+for day in ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"):
+    sales = float(input("What were the total sales for '+ day +' :"))
+    total += sales
+print("The total amount of sales for the week was: " + format(total, ",.2f"))
+print("The average amount of sales for the day was:" + format(total / 7, ",.2f"))
