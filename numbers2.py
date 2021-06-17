9e7afbcc04d5fe59817bfc3fbd5388f57030fd6d
acepele <47071340+acepele@users.noreply.github.com>
2021-06-17 11:40:05 -0500

    Add files via upload

--- numbers2.py
+++ numbers2.py
@@ -1,12 +1,10 @@
-import math
 
-PI = 3.14
 
 
 def main():
     print("This program will find the area of a shape for you.")
     menu = 0
-    while menu < 5:
+    while menu < 1 or menu > 5:
         print(" 1.Rectangle\n 2.Triangle\n 3.Square\n 4.Circle\n 5.Quit")
         menu = int(input("Please enter the number of your selection: "))
         if menu > 4:
@@ -14,18 +12,34 @@ def main():
         else:
             print("You picked " + str(menu))
 
+    if menu == 1:
+        w = float(input("Please enter the width of the rectangle in cm: "))
+        h = float(input("Please enter the height of the rectangle in cm: "))
+        area_of_rectangle(w, h)
+
+    elif menu == 2:
+        b = float(input("Enter the base of the triangle in cm: "))
+        c = float(input("Enter the height of the triangle in cm:"))
+        area_of_triangle(b, c)
+
+    elif menu == 3:
+        side = float(input("Enter the side of the square in cm: "))
+        area_of_square(side)
+
+    elif menu == 4:
+        r = float(input("Please enter the length of the radius of the circle: "))
+        area_of_circle(r)
+    else:
+        print("Quit!")
+
 
 def area_of_rectangle(width, height):
-    width = float(input("Enter the width of the rectangle in cm: "))
-    height = float(input("Enter the height of the rectangle in cm:"))
     area = width * height
     print("\n Area of a Rectangle is: %.2f" % area)
 
 
-def area_of_triangle(a, b, c):
-    b = float(input("Enter the base of the triangle in cm: "))
-    c = float(input("Enter the height of the triangle in cm:"))
-    area = (a + b + c) / 2
+def area_of_triangle(b, c):
+    area = (b * c) / 2
     print("\n Area of a Triangle is: %.2f" % area)
 
 
@@ -34,9 +48,9 @@ def area_of_square(side):
     print("\n Area of a Square is: %.2f" % area)
 
 
-def area_of_circle(pi, r):
-    radius = float(input("Please enter the length of the radius of the circle:"))
-    area = PI * r * r
+def area_of_circle(r):
+    pi = 3.14
+    area = pi * r * r
     print("\n Area of a Circle is: %.2f" % area)
 
 
