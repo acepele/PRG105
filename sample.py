f10ea9d1af068f2cba6e883507f4afccab963d80
acepele <47071340+acepele@users.noreply.github.com>
2021-06-21 13:21:17 -0500

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,26 +1,16 @@
 def main():
     sales = open("sales_totals", "r")
-    lines = sales.readlines()
-    lines = lines.rstrip("\n")
     count = 0
     total = 0
-    output()
 
 
 for line in sales:
-    print(line)
-    total += int(line)
-    count = count + 1
-    print("The sum is:", total)
-
-
-def output():
-    print("Total: ", format(total))
-    print("Number of entries: ", format(count))
-    print("Average: ", format(average))
-
-
-    sales.close()
-
+    sales_amt = float(line.rstrip('\n'))
+    print(format(sales_amt, "10, .2f"))
+    total += sales_amt
+    count += 1
+print("Total: " + format(total, "27,.2f"))
+print("Number of entries: " + format(count, "15,.2f"))
+print("Average: " + format(total / count), "25,.2f")
 
 main()
