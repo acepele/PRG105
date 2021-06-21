9b05576c667b21e03fe9aacdd75c9a4af8987781
acepele <47071340+acepele@users.noreply.github.com>
2021-06-21 10:39:01 -0500

    Add files via upload

--- contactinfo.py
+++ contactinfo.py
@@ -1,17 +1,21 @@
 def main():
     file = open('myfile.txt', 'w')
-    number = input("How many people do you want to add to the file? ")
-    name = input("What is the name of the person? ")
-    phone = input("What is their phone number? ")
-    email = input("What is their email address? ")
-
-    file.write(number + "\n")
-    file.write(name + "\n")
-    file.write(phone + "\n")
-    file.write(email + "\n")
+    file.write("information")
 
 
 
+number = input("How many people do you want to add to the file? ")
+name = input("What is the name of the person? ")
+phone = input("What is their phone number? ")
+email = input("What is their email address? ")
+
+
+
+
+
+file.write(name + "\n")
+file.write(phone + "\n")
+file.write(email + "\n")
 
 file.close()
 
