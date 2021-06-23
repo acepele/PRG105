ed1fac95a59039877218a19257cd420edc77a9c5
acepele <47071340+acepele@users.noreply.github.com>
2021-06-23 16:23:10 -0500

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,16 +1,15 @@
 def main():
-    sales = open("sales_totals", "r")
+    in_file = open("sales_totals.txt", "r")
     count = 0
     total = 0
+    for line in in_file:
+        sales_amt = float(line.rstrip('\n'))
+        print(format(sales_amt, "10,.2f"))
+        total += sales_amt
+        count += 1
+    print("Total: " + format(total, "27,.2f"))
+    print("Number of entries: " + format(count, "15,.0f"))
+    print("Average: " + format((total/count), "25,.2f"))
 
-
-for line in sales:
-    sales_amt = float(line.rstrip('\n'))
-    print(format(sales_amt, "10, .2f"))
-    total += sales_amt
-    count += 1
-print("Total: " + format(total, "27,.2f"))
-print("Number of entries: " + format(count, "15,.2f"))
-print("Average: " + format(total / count), "25,.2f")
 
 main()
