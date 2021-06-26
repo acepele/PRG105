8b05e419a52eb8e83c852081afda7cc83ec0f929
acepele <47071340+acepele@users.noreply.github.com>
2021-06-26 18:25:04 -0500

    Add files via upload

--- grades.py
+++ grades.py
@@ -1,22 +1,21 @@
 def main():
+    line = []
+    student_grades = []
+    len_list = int(input("How many students do you need to enter grades for?"))
+    for student in range(0, len_list):
+        name = input("Enter the name of student " + str(student + 1) + ": ")
+        grades = input("Enter the student's final letter grade: ")
+        line.append(name)
+        line.append(grades)
+        student_grades.append(line)
+        line = []
+    print(student_grades)
+
     outfile = open("grades.txt", "w")
-    for line in grades:
+    for line in student_grades:
         lineout = "'" + line[0] + "', '" + line[1] + "'\n"
         outfile.writelines(lineout)
     outfile.close()
-
-    len_list = int(input("How many students do you need to enter grades for?"))
-    list1 = []  # created an empty list
-    for i in range(len_list):
-        name = input("Enter the name of student: ")
-        list1.append(name)
-        print(list1)
-    for i in range(len_list):
-        grades = input("Enter the student's final letter grade: ")
-        list1.append(grades)
-        print(list1)
-
-
 
 
 main()
