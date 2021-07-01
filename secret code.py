643d1858603277eb3c9690d696e19e36f7a62a25
acepele <47071340+acepele@users.noreply.github.com>
2021-07-01 13:35:39 -0500

    Add files via upload

--- secret code.py
+++ secret code.py
@@ -1,22 +1,22 @@
 def main():
-    secret_code = ['0', 'h', '!', '_', 'f', '!', '#', '!', '$', '&', '#', 'k', 'h', '_', '*', 'k',
-                   '}', '!', '^', '#', 'm', '!', '_', ';', '!', 'h', ')', '&', '!', ';', '&', '_',
-                   '(', ')', '$', ',', 's', ')', ',', ',', '^']
+    secret_code = ['z', 'r', 's', '@', '2', '3', '4', '5', '6', '7', '8',
+                   '9', '0', '-', '=', '+', ';', '?', '>', '.', 'm', 'n',
+                   'b', 'v', 'z', ' ', 'a', 's', 'd', 'f', 'g']
 
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                'W', 'X', 'Y', 'X', 'Y', 'Z', '', ',', '.', '!', '?']
+                'W', 'X', 'Y', 'X', 'Y', 'Z', '', ',', '.', '!', '?', ' ']
 
     message = input("Please enter the message you want converted into code: ")
 
-    filename = open("secret_code.txt", 'w')
+    filename = open("secret_code.txt", "w")
     secret_code_final = ""
     for letter in message:
         for item in range(0, len(alphabet)):
             if letter.upper() == alphabet[item]:
-                secret_code_final += (secret_code[item] + " ")
-                filename.writelines(secret_code)
+                secret_code_final += (secret_code[item] + "\n")
 
+    filename.writelines(secret_code)
     print(secret_code_final)
     filename.close()
 
