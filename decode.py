3cc6b88c33556d6f8f93fde508498805b6b9c203
acepele <47071340+acepele@users.noreply.github.com>
2021-07-01 13:37:30 -0500

    Add files via upload

--- decode.py
+++ decode.py
@@ -1,30 +1,30 @@
 def main():
-    secret_code = ['0', 'h', '!', '_', 'f', '!', '#', '!', '$', '&', '#', 'k', 'h', '_', '*', 'k',
-                   '}', '!', '^', '#', 'm', '!', '_', ';', '!', 'h', ')', '&', '!', ';', '&', '_',
-                   '(', ')', '$', ',', 's', ')', ',', ',', '^']
+    secret_code = ['z', 'r', 's', '@', '2', '3', '4', '5', '6', '7', '8',
+                   '9', '0', '-', '=', '+', ';', '?', '>', '.', 'm', 'n',
+                   'b', 'v', 'z', ' ', 'a', 's', 'd', 'f', 'g']
 
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                'W', 'X', 'Y', 'X', 'Y', 'Z', '', ',', '.', '!', '?']
+                'W', 'X', 'Y', 'X', 'Y', 'Z', '', ',', '.', '!', '?', ' ']
 
-    outline = []
     print("This program will decode a coded text file.")
     filename = input("What is the name of the file to decode? ")
-    for line in filename:
-        try:
-            outfile = open("secret_code.txt", 'r')
-            for secret_code in outfile.readlines():
-                print(secret_code)
-        except ValueError:
-            print("File cannot be found")
+    solution = ""
+    outline = []
 
-            outfile = open("secret_code.txt", 'r')
-            for item in outline:
-
-
-
+    try:
+        outfile = open(filename, 'r')
+        outline = outfile.readlines()
         outfile.close()
+    except ValueError:
+        print("File cannot be found")
+    for item in outline:
+        item = item.strip('\n')
+        if item in secret_code:
+            my_index = secret_code.index(item)
+            solution += alphabet[my_index]
 
+    print(solution)
 
 
 main()
