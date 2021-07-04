f05eeaa6350e9d0ac1115b5d1dfc850280b85c6b
acepele <47071340+acepele@users.noreply.github.com>
2021-07-04 13:09:08 -0500

    Add files via upload

---
+++ rainfall.py
@@ -0,0 +1,23 @@
+def main():
+    total = 0.0
+    count = 0
+    data = open("rainfall-totals.txt", 'r')
+    for line in data:
+        line = line.rstrip("\n")
+        rain = line.split()
+        rain_month = rain[0]
+        rain_amt = rain[1].split('.')
+        data.close()
+
+        if rain_amt[0].isdigit() and rain_amt[1].isdigit():
+            amount = float(rain_amt[0] + "." + rain_amt[1])
+            total += float(amount)
+            count += 1
+        else:
+            print(rain_month + "does not have valid data because")
+            print(str(rain[1]) + "is not a number")
+    print("The total rainfall for " + str(count) + "month is " + format(total, ',.2f'))
+    print("The average rainfall was " + format((total / count), ',.2f'))
+
+
+main()
