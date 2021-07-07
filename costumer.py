b388a92382a719e28e256f0bb76d5449119a2d95
acepele <47071340+acepele@users.noreply.github.com>
2021-07-07 12:12:34 -0500

    Add files via upload

--- costumer.py
+++ costumer.py
@@ -1,21 +1,19 @@
 def main():
-    list1 = open("accounts.txt", "r")
-    list2 = open("over90.txt", "r")
-    matches = []
-    print("The following accounts are at least 90 days overdue: ")
-    for match in list1:
-        if list2 in matches:
-            matches.append(match)
-            print(matches)
     try:
         list1 = open('accounts.txt')
+        accounts = list1.readlines()
         list1.close()
         list2 = open("over90.txt")
+        over90 = list2.readlines()
         list2.close()
+        print("The following accounts are at least 90 days overdue: ")
+        for over in over90:
+            over = over.strip("\n")
+            for account in accounts:
+                if over in account:
+                    print(account)
     except FileNotFoundError:
         print("File cannot be found.")
-
-
 
 
 main()
