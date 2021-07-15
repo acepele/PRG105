6520be380454a3fd5906be0a0e4aaf6b385aabd1
acepele <47071340+acepele@users.noreply.github.com>
2021-07-15 18:18:59 -0500

    Add files via upload

--- personal_info.py
+++ personal_info.py
@@ -1,27 +1,61 @@
-class Personal:
-    def __init__(self, name, address, age, phone):
-        self.name = name
-        self.address = address
-        self.age = age
-        self.phone = phone
+def main():
+    class Personal:
+        def __init__(self, name, address, age, phone):
+            self.name = name
+            self.address = address
+            self.age = age
+            self.phone = phone
 
-    def get_name(self):
-        return self.name
+        def set_name(self, n):
+            self.name = n
 
-    def get_address(self):
-        return self.address
+        def get_name(self):
+            return self.name
 
-    def get_age(self):
-        return self.age
+        def set_address(self, a):
+            self.address = a
 
-    def get_phone(self):
-        return self.phone
+        def get_address(self):
+            return self.address
+
+        def set_age(self, g):
+            self.age = g
+
+        def get_age(self):
+            return self.age
+
+        def set_phone(self, p):
+            self.phone = p
+
+        def get_phone(self):
+            return self.phone
+
+        def display(self):
+            print("Name: ", self.get_name())
+            print("Address: ", self.get_address())
+            print("Age: ", self.get_age())
+            print("Phone: ", self.get_phone())
+
+    myself = Personal()
+    myself.set_name('Aljeda')
+    myself.set_address('507 Driftwood Ln')
+    myself.set_age('27')
+    myself.set_phone('815123456')
+    myself.display()
+
+    friend1 = Personal()
+    friend1.set_name('Mona')
+    friend1.set_address('123 Tall Grass Ln')
+    friend1.set_age('28')
+    friend1.set_phone('8156599070')
+    friend1.display()
+
+    friend2 = Personal()
+    friend2.set_name('Andy')
+    friend2.set_address('407 Country Brook Ln')
+    friend2.set_age('27')
+    friend2.set_phone('815797698008')
+    friend2.display()
 
 
-my_info = Personal("Aljeda", "507 Driftwood Ln", "27", "815123456")
-friend1 = Personal("Mona", "123 Tall Grass Ln", "28", "815678458")
-friend2 = Personal("Andy", "678 Country Brook Ln", "27", "815236890")
-
-print(my_info)
-print(friend1)
-print(friend2)
+main()
