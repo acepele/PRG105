56cfb2c650357436539742626f34c7a754fc8d08
acepele <47071340+acepele@users.noreply.github.com>
2021-07-29 14:22:34 -0500

    Add files via upload

--- gallon.py
+++ gallon.py
@@ -3,54 +3,43 @@ import tkinter
 
 class MPG:
     def __init__(self):
-        # create main window
-        self.gallons = float(self.gallons_entry.get())
         self.main_window = tkinter.Tk()
 
-        # create four frames
-        self.gallons_frame = tkinter.Frame(self.main_window)
-        self.miles_frame = tkinter.Frame(self.main_window)
-        self.mpg_frame = tkinter.Frame(self.main_window)
-        self.button_frame = tkinter.Frame(self.main_window)
+        self.top_frame = tkinter.Frame()
+        self.middle_frame = tkinter.Frame()
+        self.bottom_frame = tkinter.Frame()
 
-        self.gallons_label = tkinter.Label(self.gallons_frame, text='Enter how many gallons your car holds: ')
-        self.gallons_entry = tkinter.Entry(self.gallons_frame, width=10)
-
-        self.gallons_label.pack(side='left')
+        self.prompt_gallons = tkinter.Label(self.top_frame, text='How many gallons does your car hold: ')
+        self.gallons_entry = tkinter.Entry(self.top_frame, width=15)
+        self.prompt_gallons.pack(side='left')
         self.gallons_entry.pack(side='left')
 
-        self.miles_label = tkinter.Label(self.miles_frame, text='How many miles have you traveled: ')
-        self.miles_entry = tkinter.Entry(self.miles_frame, width=10)
-
-        self.miles_label.pack(side='left')
+        self.miles_prompt = tkinter.Label(self.middle_frame, text="How many miles did you travel? ")
+        self.miles_entry = tkinter.Entry(self.middle_frame, width=15)
+        self.miles_prompt.pack(side='left')
         self.miles_entry.pack(side='left')
 
-        self.result_label = tkinter.Label(self.mpg_frame, text=' Converted to miles Per gallons')
+        self.value = tkinter.StringVar()
+        self.results_label = tkinter.Label(self.bottom_frame, textvariable=self.value)
+        self.calc_button = tkinter.Button(self.bottom_frame, text='Calculate', command=self.calculate)
+        self.quit_button = tkinter.Button(self.bottom_frame, text='Quit', command=self.main_window.destroy)
 
-        self.mpg = tkinter.StringVar()
-        self.mpg_label = tkinter.Label(self.mpg_frame, textvariable=self.mpg)
-
-        self.result_label.pack(side='left')
-        self.mpg_label.pack(side='left')
-
-        self.calc_button = tkinter.Button(self.button_frame, text='Convert', command=self.calc_mpg)
-
-        self.quit_button = tkinter.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
-
+        self.results_label.pack(side='top')
         self.calc_button.pack(side='left')
-        self.quit_button.pack(side='left')
+        self.quit_button.pack(side='top')
 
-        self.gallons_frame.pack()
-        self.miles_frame.pack()
-        self.mpg_frame.pack()
-        self.button_frame.pack()
+        self.top_frame.pack()
+        self.middle_frame.pack()
+        self.bottom_frame.pack()
 
-    def calc_mpg(self):
-        self.gallons = float(self.gallons_entry.get())
-        self.miles = float(self.miles_entry.get())
-        # calculate MPG
-        self.miles_per_gallon = self.miles / self.gallons
-        self.mpg.set(str(round(self.miles_per_gallon, 2)))
+        tkinter.mainloop()
+
+    def calculate(self):
+        gas = float(self.gallons_entry.get())
+        miles = float(self.miles_entry.get())
+
+        mpg = "You get " + format(miles / gas, ",.2f") + "miles per gallon"
+        self.value.set(mpg)
 
 
-carmpg = MPG()
+car = MPG()
